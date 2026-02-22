7846dd7361039605020d75c2330b7af44739d7dd
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-22 21:28:36 +0600

    Now, show all jobs in UI

--- index.js
+++ index.js
@@ -12,14 +12,49 @@ const jobs = [
 
 const container = document.getElementById("jobsContainer");
 
+// render all jobs and show in UI
 function renderJobs(){
-    const card = document.createElement("div");
-    card.className = "bg-white shadow rounded-xl p-6";
+    container.innerHTML = "";
 
-    jobs.map(job => {
-        console.log(job);
+    
+    jobs.forEach(job => {
+        const {id, company, position, location, type, salary, description, status} = job;
+
+        // create new card div
+        const card = document.createElement("div");
+        card.className = "bg-white shadow rounded-xl p-6";
+        
+
+        card.innerHTML = `
+        <div class="flex justify-between">
+            <div>
+                <h3 class="font-bold text-lg">${company}</h3>
+                <p class="text-gray-700">${position}</p>
+                <p class="text-sm text-gray-500">${location} • ${type} • ${salary}</p>
+            </div>
+            <button onclick="deleteJob(${id})" class="text-red-500 font-bold">✕</button>
+        </div>
+
+        <div class="mt-3">${status}</div>
+        <p class="mt-3 text-gray-600">${description}</p>
+
+        <div class="mt-4 flex gap-3">
+            <button onclick="updateStatus(${id}, 'interview')" class="px-4 py-2 bg-green-500 text-white rounded">Interview</button>
+            <button onclick="updateStatus(${id}, 'rejected')" class="px-4 py-2 bg-red-500 text-white rounded">Rejected</button>
+        </div>
+        `;
+
+        container.appendChild(card);
     })
-}
+};
 
+// click and change job status
+function updateStatus(id){
+    // console.log('btn clicked');
+    const job = jobs.find(j => j.id === id);
+    // console.log(job);
+
+    
+}
 
 renderJobs();
